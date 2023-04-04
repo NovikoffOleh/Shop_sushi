3be2ab89b276eb13cbfc5df8247e397d00e73ccc
NovikoffOleh <novikoff10000@gmail.com>
2023-04-04 21:25:24 +0300

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,28 @@
 //делаем прослушивание событий на всей странице
 
-window.addEventListener('click', function name(params) {
+window.addEventListener('click', function name(event) {
     
+
+// проверка елемента - это Плюс?
+    if (event.target.dataset.action === 'plus') {
+     
+        const counterWrapper = event.target.closest('.counter-wrapper');
+        const counter = counterWrapper.querySelector('[data-counter]');
+        counter.innerText = ++counter.innerText 
+    }
+
+    // проверка елемента - это Минус?
+    if (event.target.dataset.action === 'minus') {
+     
+        const counterWrapper = event.target.closest('.counter-wrapper');
+        const counter = counterWrapper.querySelector('[data-counter]');
+        
+        
+        if (parseInt(counter.innerText) > 0) {
+       //уменьшаем число в счетчике на 1
+       counter.innerText = --counter.innerText 
+}
+
+    }
+
 })

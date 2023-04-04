1a2da89cd553f63044520b6db30f145e16289f6d
NovikoffOleh <novikoff10000@gmail.com>
2023-04-04 22:47:55 +0300

    correct html

--- js/counter.js
+++ js/counter.js
@@ -1,23 +1,26 @@
 //делаем прослушивание событий на всей странице
 
 window.addEventListener('click', function name(event) {
-    
 
+    let counter;
+    
+// проверка клика по конкретной кнопке а не по полю
+    
+    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') { 
+        counterWrapper = event.target.closest('.counter-wrapper');
+        counter = counterWrapper.querySelector('[data-counter]');
+    }
 // проверка елемента - это Плюс?
     if (event.target.dataset.action === 'plus') {
      
-        const counterWrapper = event.target.closest('.counter-wrapper');
-        const counter = counterWrapper.querySelector('[data-counter]');
+        
         counter.innerText = ++counter.innerText 
     }
 
     // проверка елемента - это Минус?
     if (event.target.dataset.action === 'minus') {
      
-        const counterWrapper = event.target.closest('.counter-wrapper');
-        const counter = counterWrapper.querySelector('[data-counter]');
-        
-        
+               
         if (parseInt(counter.innerText) > 0) {
        //уменьшаем число в счетчике на 1
        counter.innerText = --counter.innerText 
